98c1196fd32f4e721b31c031fa9d169e1b1bc7a4
Roman Burakov <32177478+Anvilondre@users.noreply.github.com>
2021-05-27 23:59:01 +0300

    Add first lab (#1)

---
+++ Lab01/src/main/java/lab01/Main.java
@@ -0,0 +1,23 @@
+package lab01;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+
+        Message sent_msg = new Message(1, 1, "My name is Roman!".getBytes(StandardCharsets.UTF_8));
+        Packet input = new Packet((byte) 1, 42, sent_msg);
+
+        byte[] encodedInput = Packet.encodePackage(input);
+        Packet packet = Packet.decodePackage(encodedInput);
+        String received_msg = new String(packet.message.getMsg(), StandardCharsets.UTF_8);
+        System.out.println(received_msg);
+    }
+}
